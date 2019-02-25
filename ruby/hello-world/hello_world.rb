37df9eaa1f406ef96c8cfd1c242704d2507014d9
jordanhudgens <jordan@devcamp.com>
2019-02-25 09:37:57 -0700

    Updated ruby solution

--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -1,6 +1,5 @@
 class HelloWorld
-  def self.hello(arg = nil)
-    return "Hello, #{arg}!" if arg
-    return "Hello, World!" if arg.nil?
+  def self.hello(arg = "World")
+    "Hello, #{arg}!"
   end
 end
