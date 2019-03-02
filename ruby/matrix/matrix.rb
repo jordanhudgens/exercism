570fc9fac66af62ca8123e48575673019e2fc81b
jordanhudgens <jordan@devcamp.com>
2019-03-02 13:32:21 -0600

    Finished first half of matrix exercise for ruby

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Matrix
+  def initialize(str)
+    @str = str
+  end
+
+  def rows
+    @str
+      .split("\n")
+      .map { |el| el.split(" ")
+             .map { |nested_el| nested_el.to_i }
+      }
+  end
+
+  def columns
+    # TODO
+  end
+end
+
+
+matrix = Matrix.new("1 2 3\n4 5 6\n7 8 9\n 8 7 6")
+matrix.columns
