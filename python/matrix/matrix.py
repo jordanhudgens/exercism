1512df3e0691794f807a3c153818672bb61326b8
jordanhudgens <jordan@devcamp.com>
2019-03-07 10:29:30 -0600

    Created the row based solution for the python matrix exercise

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -3,15 +3,11 @@ class Matrix(object):
         self.matrix_string = matrix_string
 
     def row(self, index):
-        split_lines = self.matrix_string("\n")
-        >>> [el.split(" ") for el in str.split("\n")]
-
-        # [['1', '2'], ['', '3', '4']]
-
-        # TODO
-        # start here and map over and convert each element to integer
-        # Then pass in the index value
-        pass
+        split_lines = self.matrix_string.split("\n")
+        list_as_strings = [el.split(" ") for el in split_lines]
+        rows = [[int(el) for el in row] for row in list_as_strings]
+        return rows[index - 1]
 
     def column(self, index):
         pass
+
