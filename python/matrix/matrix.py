ed83f8db568d3cfaa530769c9d3157bc3e895b05
jordanhudgens <jordan@devcamp.com>
2019-03-08 09:56:08 -0600

    Added column matrix feature and installed numpy

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,13 +1,15 @@
+import numpy as np
+
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
-
-    def row(self, index):
         split_lines = self.matrix_string.split("\n")
         list_as_strings = [el.split(" ") for el in split_lines]
-        rows = [[int(el) for el in row] for row in list_as_strings]
-        return rows[index - 1]
+        self.rows = [[int(el) for el in row] for row in list_as_strings]
+        self.columns = np.transpose(self.rows).tolist()
+
+    def row(self, index):
+        return self.rows[index - 1]
 
     def column(self, index):
-        pass
-
+        return self.columns[index - 1]
