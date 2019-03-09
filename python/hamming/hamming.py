f698bb6ce3d795fe2c764f32a5654bb5a201c05f
jordanhudgens <jordan@devcamp.com>
2019-03-09 13:04:26 -0600

    Refactored solution for hamming in python

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,16 +1,5 @@
 def distance(strand_a, strand_b):
-    if strand_a == strand_b:
-        return 0
-    elif len(strand_a) != len(strand_b):
-        raise ValueError(".+")
-    else:
-        return distance_value(strand_a, strand_b)
-
-def distance_value(strand_a, strand_b):
-    distance = 0
-
-    for index, char in enumerate(strand_a):
-        if char != strand_b[index]:
-            distance = distance + 1
-
-    return distance
+    """Calculate the hamming distance between two DNA sequences"""
+    if len(strand_a) != len(strand_b):
+        raise ValueError('Invalid')
+    return sum([c1 != c2 for c1, c2 in zip(strand_a, strand_b)])
