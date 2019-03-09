d8e3f1139d3b14e3ec4833018e55dae0504256bb
jordanhudgens <jordan@devcamp.com>
2019-03-09 11:00:03 -0600

    Refactored solution

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,27 +1,14 @@
-module Raindrops
-  def self.convert num
-    raindrop_string = [3, 5, 7].inject("") do |acc, i|
-      if self.divisors_of(num).include?(i)
-        if i == 3
-          acc << "Pling"
-        elsif i == 5
-          acc << "Plang"
-        else
-          acc << "Plong"
-        end
-      else
-        acc << ""
-      end
-    end
+class Raindrops
+  FACTORS = {
+    'Pling': 3,
+    'Plang': 5,
+    'Plong': 7
+  }.freeze
 
-    if raindrop_string == ""
-      raindrop_string = String(num)
-    end
-
-    raindrop_string
-  end
-
-  def self.divisors_of num
-    (1..num).select { |n| num % n == 0 }
+  def self.convert(num)
+    sounds = FACTORS.map do |sound, factor|
+      sound if (num % factor).zero?
+    end.join
+    sounds.empty? ? num.to_s : sounds
   end
 end
