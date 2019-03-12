2c740e8f084166a6aab23934ac9af04a87010df0
jordanhudgens <jordan@devcamp.com>
2019-03-12 12:27:57 -0500

    Added first part of solution for raindrop python exercise

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,9 @@
+def raindrops(number):
+    RAINDROP_TYPES = {
+        "Pling": 3,
+        "Plang": 5,
+        "Plong": 7
+    }
+
+    # >>> [k if 35 % v ==0 for k, v in RAINDROP_TYPES.items()]
+    # https://www.datacamp.com/community/tutorials/python-dictionary-comprehension
