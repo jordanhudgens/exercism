b2c7656b7d0894556d580d39d9d03f090ec2ffe3
jordanhudgens <jordan@devcamp.com>
2019-03-14 18:43:08 -0500

    Added initial implementation for isogram in python

---
+++ python/isogram/isogram.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def is_isogram(string):
+    if len(string) == 0:
+        return True
+
+    char_collection = Counter(string.replace(" ", "").replace("-", "").lower())
+
+    for k, v in char_collection.items():
+        if v > 1:
+            return False
+
