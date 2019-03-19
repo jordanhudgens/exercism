288b8f9a02817449590b4e1829a947c92ef99758
jordanhudgens <jordan@devcamp.com>
2019-03-19 11:42:04 -0400

    Added next step for the twelve days generator

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,15 +1,22 @@
 def recite(start_verse, end_verse):
-    lyric_map = { 1: ('first', 'a Partridge in a Pear Tree.'),
-                  2: ('second', 'two Turtle Doves'),
-                  3: ('third', 'three French Hens'),
-                  4: ('fourth', 'four Calling Birds'),
-                  5: ('fifth', 'five Gold Rings'),
-                  6: ('sixth', 'six Geese-a-Laying'),
-                  7: ('seventh', 'seven Swans-a-Swimming'),
-                  8: ('eight', 'eight Maids-a-Milking'),
-                  9: ('ninth', 'nine Ladies Dancing'),
-                  10: ('tenth', 'ten Lords-a-Leaping'),
+    lyric_map = { 12: ('twelfth', 'twelve Drummers Drumming'),
                   11: ('eleventh ', 'eleven Pipers Piping'),
-                  12: ('twelfth', 'twelve Drummers Drumming')
+                  10: ('tenth', 'ten Lords-a-Leaping'),
+                  9: ('ninth', 'nine Ladies Dancing'),
+                  8: ('eight', 'eight Maids-a-Milking'),
+                  7: ('seventh', 'seven Swans-a-Swimming'),
+                  6: ('sixth', 'six Geese-a-Laying'),
+                  5: ('fifth', 'five Gold Rings'),
+                  4: ('fourth', 'four Calling Birds'),
+                  3: ('third', 'three French Hens'),
+                  2: ('second', 'two Turtle Doves'),
+                  1: ('first', 'a Partridge in a Pear Tree.')
                 }
-    pass
+
+    def first_stanza(number):
+        return f"On the {number} of Christmas my true love gave to me: "
+
+    return first_stanza(5)
+
+
+print(recite(1, 2,))
