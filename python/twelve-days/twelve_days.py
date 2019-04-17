7e6badb8f0af77ba80f10ab92aa4ae107a07faab
jordanhudgens <jordan@devcamp.com>
2019-04-17 14:13:49 -0700

    Added next steps for verses

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,22 +1,24 @@
 def recite(start_verse, end_verse):
-    lyric_map = { 12: ('twelfth', 'twelve Drummers Drumming'),
-                  11: ('eleventh ', 'eleven Pipers Piping'),
-                  10: ('tenth', 'ten Lords-a-Leaping'),
-                  9: ('ninth', 'nine Ladies Dancing'),
-                  8: ('eight', 'eight Maids-a-Milking'),
-                  7: ('seventh', 'seven Swans-a-Swimming'),
-                  6: ('sixth', 'six Geese-a-Laying'),
-                  5: ('fifth', 'five Gold Rings'),
-                  4: ('fourth', 'four Calling Birds'),
-                  3: ('third', 'three French Hens'),
-                  2: ('second', 'two Turtle Doves'),
+    lyric_map = { 12: ('twelfth', 'twelve Drummers Drumming, '),
+                  11: ('eleventh ', 'eleven Pipers Piping, '),
+                  10: ('tenth', 'ten Lords-a-Leaping, '),
+                  9: ('ninth', 'nine Ladies Dancing, '),
+                  8: ('eight', 'eight Maids-a-Milking, '),
+                  7: ('seventh', 'seven Swans-a-Swimming, '),
+                  6: ('sixth', 'six Geese-a-Laying, '),
+                  5: ('fifth', 'five Gold Rings, '),
+                  4: ('fourth', 'four Calling Birds, '),
+                  3: ('third', 'three French Hens, '),
+                  2: ('second', 'two Turtle Doves, '),
                   1: ('first', 'a Partridge in a Pear Tree.')
                 }
 
-    def first_stanza(number):
-        return f"On the {number} of Christmas my true love gave to me: "
+    def verse_builder(number):
+        first_stanza = f"On the {number} of Christmas my true love gave to me: "
+        # TODO
+        # build list comprehension for all verses
 
-    return first_stanza(5)
+    return verse_builder(lyric_map[start_verse][0])
 
 
 print(recite(1, 2,))
