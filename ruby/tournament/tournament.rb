1d9d2ceb7e5fbb13a827a14e318c8fd027dfddff
jordanhudgens <jordan@devcamp.com>
2019-04-24 12:59:27 -0700

    Added next step of the tourny project

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,4 +1,4 @@
-# MP: Matches Played
+ MP: Matches Played
 # W: Matches Won
 # D: Matches Drawn (Tied)
 # L: Matches Lost
@@ -6,19 +6,24 @@
 # A win earns a team 3 points. A draw earns 1. A loss earns 0.
 
 class Tournament
-  attr_accessor :tally
+  attr_accessor :tournament_data
 
   def initialize(input)
     # have program automatically generate the score
     # and store in the tally value,
     # need to keep track of all teams since they
     # can show up multiple times
+    self.tournament_data = input
   end
 
   def self.tally(input)
     scoreboard = <<~TALLY
     Team                           | MP |  W |  D |  L |  P
     TALLY
+  end
+
+  def team_builder
+    self.tournament_data
   end
 
   def self.score_generator(game)
@@ -35,6 +40,17 @@ end
 
 input = <<~INPUT
 Allegoric Alaskans;Blithering Badgers;win
+Devastating Donkeys;Courageous Californians;draw
+Devastating Donkeys;Allegoric Alaskans;win
+Courageous Californians;Blithering Badgers;loss
+Blithering Badgers;Devastating Donkeys;loss
+Allegoric Alaskans;Courageous Californians;win
 INPUT
 
-p Tournament.tally(input)
+# p Tournament.tally(input)
+
+# Team                           | MP |  W |  D |  L |  P
+# Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3
+# Blithering Badgers             |  1 |  0 |  0 |  1 |  0
+
+Tournament.new(input).team_builder
