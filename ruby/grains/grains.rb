8cefb97cd7cac20606ef8f2ebfd62924f42f879f
jordanhudgens <jordan@devcamp.com>
2019-05-07 10:06:35 -0700

    Refactored grains solution

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,24 +1,12 @@
-module Grains
-  def self.square(num)
-    raise ArgumentError if (num <= 0) || (num > 64)
-    return 1 if num == 1
-    self.doubler(num)
-  end
-
-  def self.doubler(num)
-    (1..(num - 1)).to_a.inject(0) do |sum, el|
-      if sum == 0
-        sum += 2
-      else
-        sum = sum * 2
-      end
-    end
+class Grains
+  def self.square n
+    raise ArgumentError unless n.between?(1, 64)
+    2 ** (n - 1)
   end
 
   def self.total
-    (1..64).to_a.inject(1) do |sum, el|
-      sum += self.doubler(el)
-    end
+    (1..64)
+      .map { |i| self.square(i) }
+      .sum
   end
 end
-
