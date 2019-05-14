517af13e47a162b7a20c9903e0932b099bdb2b21
jordanhudgens <jordan@devcamp.com>
2019-05-14 12:42:30 -0700

    Started refactor of the verse builder

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,5 +1,5 @@
-def recite(start_verse, number_of_days):
-    if (start_verse == 1 and number_of_days == 1):
+def recite(start_verse, number_of_repetition):
+    if (start_verse == 1 and number_of_repetition == 1):
         return ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."]
 
     lyric_map = { 12: ('twelfth', 'twelve Drummers Drumming, '),
@@ -21,14 +21,22 @@ def recite(start_verse, number_of_days):
         day_range = list(range(1, 13))
         day_range.reverse()
         index_of_start_day = day_range.index(start_verse)
-        selected_days = day_range[index_of_start_day:(index_of_start_day + number_of_days)]
+        selected_days = day_range[index_of_start_day:(index_of_start_day + start_verse)]
 
         for day in selected_days:
             verse = verse + lyric_map[day][1]
 
         return [verse]
 
+    # TODO
+    # iterate over the number of repetitions
     return verse_builder(lyric_map[start_verse][0])
 
-#  print(recite(11, 11))
+
+#  expected = [recite(n, n)[0] for n in range(1, 4)]
 #  print(expected)
+#  ['On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.', 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.', 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.']
+
+#  print(recite(1, 3))
+# ['On the first day of Christmas my true love gave to me: and a Partridge in a Pear Tree.']
+
