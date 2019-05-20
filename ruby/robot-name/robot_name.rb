cca32230cb6514ec664dd76f89e762fd302ff951
jordanhudgens <jordan@devcamp.com>
2019-05-20 09:03:32 -0700

    Added new file and file adding process

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,36 +1,35 @@
-require 'date'
+require 'date'  # => true
 
 class Robot
-  attr_accessor :name
+  attr_accessor :name  # => nil
 
   def initialize
-    @name = name_generator
-  end
+    @name = name_generator  # => nil, nil
+  end                       # => :initialize
 
   def name_generator
-    two_letters = ("A".."Z").to_a.sample(2).join("")
-    three_numbers = random_numbers
-    two_letters << three_numbers
-  end
+    two_letters = ("A".."Z").to_a.sample(2).join("")  # => "ZQ",              "UL"
+    three_numbers = random_numbers                    # => "379",             "379"
+    name = two_letters << three_numbers               # => "ZQ379",           "UL379"
+    # get file reader working
+    f = File.open("names.txt", 'a')                   # => #<File:names.txt>, #<File:names.txt>
+    f.puts(name)                                      # => nil,               nil
+    f.close                                           # => nil,               nil
+  end                                                 # => :name_generator
 
   def random_numbers
-    DateTime.now.strftime('%Q').split("").last(3).join("")
-  end
+    DateTime.now.strftime('%Q').split("").last(3).join("")  # => "379", "379"
+  end                                                       # => :random_numbers
 
   def self.forget
     # TODO
-  end
+  end              # => :forget
 
   def reset
     @name = name_generator
-  end
-end
+  end                       # => :reset
+end                         # => :reset
 
-# TODO
-# save robot names in a file
-# then check through file to ensure name hasn't been used
-#
-
-Robot.new.name # => "UI502"
-Robot.new.name # => "GL502"
+Robot.new.name  # => nil
+Robot.new.name  # => nil
 
