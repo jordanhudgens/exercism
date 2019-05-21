2c2d853c56060649876551614e1c7a65fd812ad3
jordanhudgens <jordan@devcamp.com>
2019-05-21 09:29:26 -0700

    Created the ability to save to a file

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -4,21 +4,21 @@ class Robot
   attr_accessor :name  # => nil
 
   def initialize
-    @name = name_generator  # => nil, nil
+    @name = name_generator  # => 5, 5
   end                       # => :initialize
 
   def name_generator
-    two_letters = ("A".."Z").to_a.sample(2).join("")  # => "ZQ",              "UL"
-    three_numbers = random_numbers                    # => "379",             "379"
-    name = two_letters << three_numbers               # => "ZQ379",           "UL379"
+    two_letters = ("A".."Z").to_a.sample(2).join("")    # => "SL",    "SF"
+    three_numbers = random_numbers                      # => "222",   "228"
+    name = two_letters << three_numbers                 # => "SL222", "SF228"
     # get file reader working
-    f = File.open("names.txt", 'a')                   # => #<File:names.txt>, #<File:names.txt>
-    f.puts(name)                                      # => nil,               nil
-    f.close                                           # => nil,               nil
-  end                                                 # => :name_generator
+    File.open("ruby/robot-name/names.txt", 'a') do |f|  # => File, File
+      f.write(name)                                     # => 5,    5
+    end                                                 # => 5,    5
+  end                                                   # => :name_generator
 
   def random_numbers
-    DateTime.now.strftime('%Q').split("").last(3).join("")  # => "379", "379"
+    DateTime.now.strftime('%Q').split("").last(3).join("")  # => "222", "228"
   end                                                       # => :random_numbers
 
   def self.forget
@@ -30,6 +30,6 @@ class Robot
   end                       # => :reset
 end                         # => :reset
 
-Robot.new.name  # => nil
-Robot.new.name  # => nil
+Robot.new.name  # => 5
+Robot.new.name  # => 5
 
