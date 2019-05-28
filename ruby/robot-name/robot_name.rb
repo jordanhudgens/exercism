cc3464b748e7cd594292961540936faf16024743
jordanhudgens <jordan@devcamp.com>
2019-05-28 09:59:55 -0600

    Refactored naming solution and added todo

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,34 +1,24 @@
+$numbers = ("000".."999").to_a
+$letters = ("AA".."ZZ").to_a
 $names = []
 
 class Robot
   attr_accessor :name
-  LETTERS = ('A'..'Z').to_a.freeze
-  private_constant :LETTERS
 
   def initialize
     @name = name_builder
   end
 
   def name_builder
-    unique = false
-
-    until unique do
-      temp_name = run_name_generator
-
-      if !$names.include?(temp_name)
-        $names << temp_name
-        unique = true
-      end
-    end
-
-    $names.last
-  end
-
-  def run_name_generator
-    '%s%s%03d' % [LETTERS.sample, LETTERS.sample, rand(100)]
+    # TODO
+    # Need to get through the numbers before going through the letters
+    generated_name = $letters.shift + $numbers.shift
+    $names << generated_name
+    generated_name
   end
 
   def self.forget
+    $names = []
   end
 
   def reset
